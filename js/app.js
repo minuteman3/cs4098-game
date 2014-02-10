c16adec49d9f12974314c5799699f46e49ed8126
Ross McKinley <wrossmck@gmail.com>
2014-02-10 17:34:45 +0000

    added comments

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ var pt = (function () {
       constructor: pt
   };
 
+  // this file will export all of the functions in this Module var below.
   var Module = { 
     makemap: function() {
       var map;
+
+      // This is a jvectormap object, which has terrible docs.
+      // nearly all settings were inferred from the examples @ http://jvectormap.com/examples/regions-selection/
       map = new jvm.WorldMap({
         width: 720,
         heigth: 300,
@@ -27,6 +31,8 @@ var pt = (function () {
         regionsSelectable: true,
         backgroundColor: "transparent",
         onRegionSelected: function(){
+          // this enables persistent data of the selected regions
+          // it gets called each time a region is selected.
           if (window.localStorage) {
             window.localStorage.setItem(
               'jvectormap-selected-regions',
@@ -35,10 +41,12 @@ var pt = (function () {
           }
         }
       });
+      // this gets persistent data of the selected regions, and puts them on the map at page load.
       map.setSelectedRegions( JSON.parse( window.localStorage.getItem('jvectormap-selected-regions') || '[]' ) );
     },
 
     region: function(item){
+      // this function will be modified later to return the data only.
       item = item || 'jvectormap-selected-regions';
       var regions = window.localStorage.getItem(item);
       var r = "Regions selected: "+regions;
--- tests/browser/test-maps-loading.js
+++ tests/browser/test-maps-loading.js
@@ -2,7 +2,7 @@ var assertion = 0;
 console.log('TAP version 13');
 console.log('1..2');
 
-
+// this is a "faux-TAP-outputter", made so that testling can interpret it.
 console.log('# localStorage is enabled');
 window.localStorage.setItem(
               'test-local-storage',
@@ -19,5 +19,6 @@ equal(pt.region('test-local-storage'), "hi");
 
 function text(elem) { return elem.textContent || elem.innerText }
 function equal (a, b) {
+	// output must be in this format (either 'ok', or 'not ok') followed by the number of assertions which have occurred.
     console.log((a === b ? 'ok' : 'not ok') + ' ' + (++ assertion));
 }
