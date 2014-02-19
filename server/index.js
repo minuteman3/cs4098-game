9e7023c2671fd7f0cd2308fe295d72d919dee433
Miles McGuire <miles@milesmcguire.com>
2014-02-19 02:50:13 +0000

    Build stylesheets on demand
    Removes unnecessary dependency on less CLI

--- server/index.js
+++ server/index.js
@@ -2,15 +2,22 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var config = require ('./config-loader.js');
+var browserify = require('browserify');
+var b = browserify(config.APP_DIR + "js/index.js");
+b.transform('lessify');
+
 
 module.exports = null;
 
 http.createServer(function (req,res) {
-    var result = /^\/?(lib|css|js)\/.+$/.exec(req.url);
+    var result = /^\/?(lib|js)\/.+$/.exec(req.url);
     if (req.url === "/config") {
         res.writeHead(200);
         res.end("PANIC TYCOON CONFIG\n===================\n\n" + JSON.stringify(config));
         return;
+    } else if (req.url === "/js/bundle.js") {
+        res.writeHead(200);
+        b.bundle().pipe(res);
     } else if (result) {
         res.writeHead(200);
         res.end(fs.readFileSync(path.normalize(config.APP_DIR + result[0])));
