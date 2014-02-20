b6d1ac8fef3bffcabe22b765813e82955e42cc2f
Miles McGuire <miles@milesmcguire.com>
2014-02-20 17:15:54 +0000

    Simplify server after move to more bundling with browserify

--- server/index.js
+++ server/index.js
@@ -4,13 +4,8 @@ var path = require('path');
 var config = require ('./config-loader.js');
 var browserify = require('browserify');
 var b = browserify(config.APP_DIR + "js/index.js");
-//b.transform('lessify');
-
-
-module.exports = null;
 
 http.createServer(function (req,res) {
-    var result = /^\/?(lib|js)\/.+$/.exec(req.url);
     if (req.url === "/config") {
         res.writeHead(200);
         res.end("PANIC TYCOON CONFIG\n===================\n\n" + JSON.stringify(config));
@@ -18,10 +13,6 @@ http.createServer(function (req,res) {
     } else if (req.url === "/js/bundle.js") {
         res.writeHead(200);
         b.bundle().pipe(res);
-    } else if (result) {
-        res.writeHead(200);
-        res.end(fs.readFileSync(path.normalize(config.APP_DIR + result[0])));
-        return;
     } else {
         res.writeHead(200);
         res.end(fs.readFileSync(path.normalize(config.APP_DIR + "index.html")));
