3c47918dbe068b4b35b21b419308843acdf38304
Kevin Farrell <kevin@Kevins-MacBook-Pro.local>
2014-02-22 23:11:21 +0000

    changed select regions to select teams

--- js/app.js
+++ js/app.js
@@ -4,34 +4,41 @@ var jvm = require('jvm');
 var jvm_map = require('../lib/jquery-jvectormap-world-mill-en.js')();
 var map;
 
+var teamsSelected = {};
+var teamCost = 0;
+
+var makerclasses = "jvectormap-marker jvectormap-element ";
+
+
+
+
+
 function buildmap (){
   resizemap();
   // This is a jvectormap object, which has terrible docs, but good examples
   // nearly all settings were inferred from the examples @ http://jvectormap.com/examples/regions-selection/
   map = new jvm.WorldMap({
     map: 'world_mill_en',
-    markers: cities.metro.coords,
+    markers: cities.coords,
     container: $('#map'),
-    regionStyle: {
-      initial: {
-        fill: '#2aa198' //cyan
-      }
-    },
     series:{
       markers: [{
-        attribute: 'fill',
-        scale: ['#A50F15', '#FEE5D9'],
-        values: cities.metro.unemployment[2009]
+        attribute: 'r',
+        scale: [6, 8],
+        values: cities.productivity
       }]
     },
     backgroundColor: 'transparent',
-    onRegionSelected: regionSelect,
+    onMarkerSelected: teamSelected,
     onMarkerLabelShow: onlabelShow,
-    markersSelectable: true
+    markersSelectable: true,
+    onRegionLabelShow:regionLabelShow,
+
   });
   // this gets persistent data of the selected regions, and puts them on the map at page load.
   // map.setSelectedMarkers();
 }
+
 
 function displayregion () {
   var regions = region();
@@ -39,13 +46,19 @@ function displayregion () {
   document.getElementById('regions').innerHTML = r;
 }
 
+function regionLabelShow(e,label,code){ 
+  label.css('visibility','hidden');
+}
+
+
 function onlabelShow (e,label,code){
+  label.css('visibility','visible');
     label.html(
-      cities.metro.names[code]+'<br/>'+
-      'Morale: '+            cities.metro.unemployment[2009][code] +'%<br/>'+
-      'Productivity: '+      cities.metro.unemployment[2009][code] +'%<br/>'+
-      'Cost per cycle: '+    cities.metro.unemployment[2009][code] +'%<br/>'+
-      'Unemployment rate: '+ cities.metro.unemployment[2009][code] +'%'
+      cities.names[code]+'<br/>'+
+      'Morale: '+            cities.morale[code] +'%<br/>'+
+      'Productivity: '+      cities.productivity[code] +'%<br/>'+
+      'Monthly cost: $'+    cities.costPerCycle[code] +'<br/>'+
+      'Unemployment rate: '+ 0 +'%'
     );
 }
 
@@ -55,17 +68,32 @@ function region (item) {
   return regions;
 }
 
-function regionSelect () {
+function teamSelected (e,  code,  isSelected,  selectedMarkers) {
+  if(!(code in teamsSelected))
+    teamsSelected[code] = 1;
+  else 
+    teamsSelected[code] += 1; ;
+
+  var locations = teamsSelected;
+  //selectedMarkers.map(function(i){return cities.names[i];});
   // this enables persistent data of the selected regions
   // it gets called each time a region is selected.
   if (window.localStorage) {
-    window.localStorage.setItem('jvectormap-selected-regions', JSON.stringify(map.getSelectedRegions().sort()));
+    window.localStorage.setItem('jvectormap-selected-regions', JSON.stringify(locations));
   }
   var regionsHtml = '';
-  $.each(map.getSelectedRegions().sort(), function(a, b) {
-    regionsHtml += '<li>' + b + '</li>';
-  });
+  var cost = 0;
+  for(var key in teamsSelected){
+    regionsHtml += '<li>';
+    regionsHtml += cities.names[key];
+    regionsHtml += "<div class='teamMultiplier'>x" + teamsSelected[key] + "</div>";
+    if (key === code) regionsHtml += "<div class='teamMultiplierFade'>+</div>";
+    regionsHtml += '</li>';
+    cost += cities.costPerCycle[key] *  teamsSelected[key];
+  }
+  teamCost = cost;
   $('#locations').html(regionsHtml);
+  $('#costPerMonth').html("$" + cost);
 }
 
 function resizemap (s) {
@@ -73,8 +101,9 @@ function resizemap (s) {
   document.getElementById('map').style.height = (document.documentElement.clientHeight * s / 100) + 'px';
 }
 
-function selectRegions () {
-  map.clearSelectedRegions();
+function selectTeams () {
+  teamsSelected = {};
+  map.clearSelectedMarkers();
   window.localStorage.setItem('jvectormap-selected-regions', []);
   var index = $('.active').index('li');
   $('#locations').html('');
@@ -84,11 +113,14 @@ function selectRegions () {
     index += 2;
     $('.active').removeClass('active');
     $('.nav li:nth-child(' + index + ')').addClass('active');
+
+    $('.nav li:nth-child(' + index + ')').addClass('active');
+    $('#costPerMonth').html("$0");
   }
 }
 
 module.exports = {
     makemap: buildmap,
-    selectRegions: selectRegions,
+    selectTeams: selectTeams,
     region: region
 };
