67526f87441f947c3b3245f1af4893bd8618b4d2
Ross McKinley <wrossmck@gmail.com>
2014-02-25 16:50:29 +0000

    added test for browser supporting foreach

--- tests/browser/test-maps-loading.js
+++ tests/browser/test-maps-loading.js
@@ -3,18 +3,30 @@ var app  = require('../../js/index.js');
 
 
 test('localStorage is enabled', function (t) {
-    t.plan(2);
-    t.doesNotThrow(function () {
-		window.localStorage.setItem(
-              'test-local-storage',
-              "hi");
-    });
-    t.equal(window.localStorage.getItem('test-local-storage'), "hi");
+  t.plan(2);
+  t.doesNotThrow(function () {
+	window.localStorage.setItem(
+            'test-local-storage',
+            "hi");
+  });
+  t.equal(window.localStorage.getItem('test-local-storage'), "hi");
 });
 
 test('pt can be accessed', function (t) {
-    t.plan(1);
-    t.doesNotThrow(function () {
-      pt.initialiseGame();
+  t.plan(1);
+  t.doesNotThrow(function () {
+    pt.initialiseGame();
+  });
+});
+
+test('browser supports forEach', function(t){
+  t.plan(2);
+  var a = ["a","b","c"];
+  t.doesNotThrow(function(){
+    a.forEach(function(b, i, arr){
+      b = b.toUpperCase();
+      arr[i] = b;
     });
+  });
+  t.deepEqual(a,["A","B","C"]);
 });
