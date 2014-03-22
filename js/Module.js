2c772dd632f2b401340eb198b0abcdc596b6004f
Miles McGuire <miles@milesmcguire.com>
2014-03-22 13:01:40 +0000

    Add a 'stall' function to Module.prototype that can be invoked to pause progress on a module for N weeks

--- js/Module.js
+++ js/Module.js
@@ -2,40 +2,48 @@ var Module = function(_developersPerCity, _cost){
     this.progress = 0;
     this.developersPerCity = _developersPerCity;
     this.cost = _cost + _cost*0.25*(Math.random() - 0.5);
+    this.stalled = 0;
 };
 
-Module.prototype.getPercentComplete = function(){
-	return (this.progress/this.cost)*100;
+Module.prototype.getPercentComplete = function getPercentComplete () {
+    return (this.progress/this.cost)*100;
 };
 
-Module.prototype.done = function(){
-	return this.progress >= this.cost;
+Module.prototype.done = function done () {
+    return this.progress >= this.cost;
 };
 
-Module.prototype.advance = function(cities){
-	if(!this.done()){
-		var progressThisCycle = 0;
-		var devs = this.developersPerCity;
-		Object.keys(devs).forEach(function(key) {
-			progressThisCycle += cities[key].progress(devs[key]);
-		});
-
-		this.progress += progressThisCycle;
-
-		if(this.progress > this.cost){
-			this.progress = this.cost;
-		}
-	}
+Module.prototype.stall = function stall (duration) {
+    this.stalled = duration;
 };
 
-Module.prototype.getCost = function(cities){
-	var devs = this.developersPerCity;
-	var cost = 0;
-	Object.keys(this.developersPerCity).forEach(function(key) {
-		var developers = devs[key];
-		cost += cities[key].cost(developers);
-	});
-	return cost;
+Module.prototype.advance = function advance (cities) {
+    if (this.stalled > 0) {
+        this.stalled --;
+        return;
+    } else if(!this.done()) {
+        var progressThisCycle = 0;
+        var devs = this.developersPerCity;
+        Object.keys(devs).forEach(function(key) {
+            progressThisCycle += cities[key].progress(devs[key]);
+        });
+
+        this.progress += progressThisCycle;
+
+        if(this.progress > this.cost){
+            this.progress = this.cost;
+        }
+    }
+};
+
+Module.prototype.getCost = function getCost (cities) {
+    var devs = this.developersPerCity;
+    var cost = 0;
+    Object.keys(this.developersPerCity).forEach(function(key) {
+        var developers = devs[key];
+        cost += cities[key].cost(developers);
+    });
+    return cost;
 };
 
 module.exports = Module;
--- tests/test-module.js
+++ tests/test-module.js
@@ -2,25 +2,39 @@ var test = require('tape');
 var Module = require('../js/Module.js');
 var City = require("../js/city.js");
 
-test('Module advancement works', function(t) 
+test('Module advancement works', function(t)
 {
     t.plan(2);
 
     var mod = new Module(
-				{
-					"Dublin": 1,
-					"Mumbai": 1
-				},
-				300
-				);
+                {
+                    "Dublin": 1,
+                    "Mumbai": 1
+                },
+                300
+                );
     var citiesState = {
-			"Dublin" : new City("Dublin",6000,100),
-			"Mumbai" : new City("Mumbai",500,100)
-		};
-	t.doesNotThrow(function(){
+            "Dublin" : new City("Dublin",6000,100),
+            "Mumbai" : new City("Mumbai",500,100)
+        };
+    t.doesNotThrow(function(){
         mod.advance(citiesState);
-	});
+    });
     // 100%/300(±25%)*Developers*100%
     t.ok(53 < mod.getPercentComplete() && mod.getPercentComplete() < 89 , 'project cost randomisation');
 });
 
+test('Modules can be stalled', function (t) {
+    t.plan(2);
+    var mod = new Module({
+                    Dublin: 1
+                  }, 300);
+    var citiesState = {
+                Dublin: new City("Dublin",6000,100)
+        };
+    mod.stall(1);
+    mod.advance(citiesState);
+    t.ok(mod.getPercentComplete() === 0, 'module has not advanced when stalled');
+    mod.advance(citiesState);
+    t.ok(mod.getPercentComplete() > 0, 'module unstalls on its own');
+});
