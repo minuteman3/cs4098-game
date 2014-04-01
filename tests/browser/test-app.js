a43430f12ed2c17955e7c0850e17fe67ea171d91
Ross McKinley <wrossmck@gmail.com>
2014-04-01 17:38:11 +0100

    placeholders for tests

--- tests/browser/test-app.js
+++ tests/browser/test-app.js
@@ -7,3 +7,39 @@ test('pt can be accessed', function (t) {
 		pt.initialiseGame();
 	});
 });
+
+test('endGame', function (t) {
+    t.plan(1);
+	t.doesNotThrow(function(){
+		pt.initialiseGame();
+	});
+});
+
+test('projectdescription', function (t) {
+    t.plan(1);
+	t.doesNotThrow(function(){
+		pt.initialiseGame();
+	});
+});
+
+test('selectProject', function (t) {
+    t.plan(1);
+	t.doesNotThrow(function(){
+		pt.initialiseGame();
+	});
+});
+
+test('startGame', function (t) {
+    t.plan(1);
+	t.doesNotThrow(function(){
+		pt.initialiseGame();
+	});
+});
+
+test('startSimulation', function (t) {
+    t.plan(1);
+	t.doesNotThrow(function(){
+		pt.initialiseGame();
+	});
+});
+
