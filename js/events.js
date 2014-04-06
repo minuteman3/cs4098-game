5c153acf7db7cf0384792dc90f846df155160a1b
Ross McKinley <wrossmck@gmail.com>
2014-04-06 15:48:28 +0100

    removed unused methods

--- js/events.js
+++ js/events.js
@@ -1,14 +1,11 @@
 var eventData = require("../config/events.json");
 var utils     = require('./utils.js');
+var dc        = require('deepcopy');
 
 var events = [];
 
 function setEventAction(num){
-  events[events.length-1].mitigation = events[events.length-1].actions[num];
-}
-
-function getEvents(){
-  return events;
+  events[events.length-1].mitigation = dc(events[events.length-1].actions[num]);
 }
 
 function trackEvent(ev){
@@ -18,11 +15,10 @@ function trackEvent(ev){
 function doEvent(actionNumber,gamedata){
   setEventAction(actionNumber);
 
-  var ev = getEvents();
+  var ev = events;
   var cev = ev[ev.length-1];
   var effects = utils.objectadd(cev.effects, cev.actions[actionNumber].effects);
   var city = cev.city;
-
   var moneyEffect = 0;
 
   if(effects.money){
@@ -43,16 +39,13 @@ function doEvent(actionNumber,gamedata){
     //   city.setMorale(0-effects.morale);
     // },5000);
   }
-  if(effects.progress)
-  {
+  if(effects.progress){
       cev.module.setPercentageComplete(effects.progress);
   }
 }
 
 module.exports = {
     //events
-    setEventAction: setEventAction,
-    getEvents: getEvents,
     trackEvent:trackEvent,
     doEvent:doEvent,
 };
