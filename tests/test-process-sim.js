2786730531dc7f5a19578b602093ac8d616517a3
Tom Mason <wheybags@wheybags.com>
2014-04-07 13:32:14 +0100

    fixed process sim test

--- tests/test-process-sim.js
+++ tests/test-process-sim.js
@@ -7,45 +7,63 @@ var Module = require("../js/Module.js");
 var City = require("../js/city.js");
 
 test("process simulator: works", function(t) {
-	t.plan(6);
+	t.plan(5);
 	var citiesState={};
-	var modules = [];
+    var modules = [new Module({"Dublin": 1, "Mumbai": 1}, 400, "test")];
+
 	t.doesNotThrow(function(){
-		// citiesState = {c.name: new City(c.name,c.costPerWeek,c.productivity) };
+        var dublin = 
+        {
+            "coords": [
+                37.6788056,
+                -122.2880726
+            ],
+            "name": "Dublin",
+            "morale": 100,
+            "productivity": 200,
+            "costPerWeek": 12000,
+            "language": "english",
+            "west": true,
+            "highContext": false,
+            "nationCulture": "american",
+            "organizationalCulture": "undefined"
+        };
+
+        var mumbai = 
+        {
+            "coords": [
+                40.43,
+                -74
+            ],
+            "name": "Mumbai",
+            "morale": 100,
+            "productivity": 125,
+            "costPerWeek": 7500,
+            "language": "english",
+            "west": true,
+            "highContext": false,
+            "nationCulture": "american",
+            "organizationalCulture": "undefined"
+        };
+
+        
 		citiesState = {
-			"Dublin" : new City("Dublin",6000,100),
-			"Mumbai" : new City("Mumbai",500,100)
+            "Dublin" : new City(mumbai, dublin, modules),
+			"Mumbai" : new City(dublin, dublin, modules)
 		};
 	},'add cities');
+	
+    var rate = config.eventRate;
 	t.doesNotThrow(function(){
-		modules.push(
-			new Module( 
-				{
-					"Dublin": 1,
-					"Mumbai": 1
-				},
-				400,
-                "test"
-			)
-		); 
-	},'add modules');
-	var rate = config.eventRate;
-	t.doesNotThrow(function(){
-	// function start(_modules,_cities, _updateFunc, _doneFunc, _eventFunc, events){
 		ProcessSim.start(modules, citiesState,function(){}, function() {
 			var done = true;
-
+            
 			modules.forEach(function(module) {
 				done = done && module.done();
 			});
 			
-			// at this point, done should still be true, or else timerLoop is borken
+            // at this point, done should still be true, or else timerLoop is borken
 			t.ok(done, 'timerLoop()');
-
-			// t.deepEqual(ProcessSim.getCity("Dublin"),citiesState.Dublin,"getCity()");
-			// var testc = new City("test",222,111);
-			// ProcessSim.setCity("test",testc);
-			// t.deepEqual(ProcessSim.getCity("test"),testc,"setCity()");
 
 			t.doesNotThrow(function(){
 				ProcessSim.pause();
