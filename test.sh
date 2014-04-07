a41fb6fbc4c014d4acbab0b9e53106418452d2e7
Tom Mason <wheybags@wheybags.com>
2014-04-07 15:47:38 +0100

    upper case filename

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ FUNCTIONS=($(grep -r -o "function \w\w*" "function\s*(\w\w*)" js | grep -oE '[^ 
 for function  in "${FUNCTIONS[@]}"
 do
 	if ! grep -r --quiet $function tests ; then
-		if ! grep --quiet -e $function not_tested.csv ; then
+		if ! grep --quiet -e $function NOT_TESTED.csv ; then
 	  		echo $function
 		fi
 	fi
