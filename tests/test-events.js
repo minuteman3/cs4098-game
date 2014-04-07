eb858c7033f8920eee87bede31ba6eae810ab3f4
Tom Mason <wheybags@wheybags.com>
2014-04-07 13:50:48 +0100

    fix event test

--- tests/test-events.js
+++ tests/test-events.js
@@ -1,9 +1,48 @@
 var test = require('tape');
 var events = require("../js/events.js");
 var City = require("../js/city.js");
+var Module = require("../js/Module.js");
 
 test("events:", function(t){
     t.plan(2);
+
+    var city = 
+    {
+        "coords": [
+            37.6788056,
+            -122.2880726
+        ],
+        "name": "San Francisco",
+        "morale": 100,
+        "productivity": 200,
+        "costPerWeek": 12000,
+        "language": "english",
+        "west": true,
+        "highContext": false,
+        "nationCulture": "american",
+        "organizationalCulture": "undefined"
+    };
+
+    var homeCity = 
+    {
+        "coords": [
+            40.43,
+            -74
+        ],
+        "name": "New York",
+        "morale": 100,
+        "productivity": 125,
+        "costPerWeek": 7500,
+        "language": "english",
+        "west": true,
+        "highContext": false,
+        "nationCulture": "american",
+        "organizationalCulture": "undefined"
+    };
+
+    var modules = [new Module({"dublin": 1}, 100, "test")];
+    var c = new City(city, homeCity, modules);
+ 
 
     var e = {
         "conditions": {},
@@ -17,7 +56,7 @@ test("events:", function(t){
                 "effects": {}
             }
         ],
-        "city": new City("Dublin", 300, 30),
+        "city": c,
         "module":{
             "name": "test"
         }
