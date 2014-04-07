547e26eef37edf15d0e7ce8c46ac6b7a91358340
Tom Mason <wheybags@wheybags.com>
2014-04-07 13:19:16 +0100

    fixed city tests

--- tests/test-city.js
+++ tests/test-city.js
@@ -1,34 +1,62 @@
 var test = require('tape');
-var city = require("../js/city.js");
+var Module = require("../js/Module.js");
+var City = require("../js/city.js");
 
 test('city: class works', function (t) {
-    t.plan(4);
+    t.plan(13);
+
+    var city = 
+    {
+        "coords": [
+            37.6788056,
+            -122.2880726
+        ],
+        "name": "San Francisco",
+        "morale": 100,
+        "productivity": 200,
+        "costPerWeek": 12000,
+        "language": "english",
+        "west": true,
+        "highContext": false,
+        "nationCulture": "american",
+        "organizationalCulture": "undefined"
+    };
+
+    var homeCity = 
+    {
+        "coords": [
+            40.43,
+            -74
+        ],
+        "name": "New York",
+        "morale": 100,
+        "productivity": 125,
+        "costPerWeek": 7500,
+        "language": "english",
+        "west": true,
+        "highContext": false,
+        "nationCulture": "american",
+        "organizationalCulture": "undefined"
+    };
 
     var c = null;
     t.doesNotThrow(function () {
-        c = new city("dublin", 100, 200);
+        var modules = [new Module({"dublin": 1}, 100, "test")];
+        c = new City(city, homeCity, modules);
     });
-
-    t.equal(c.costPerDeveloper, 100);
+    
+    t.equal(c.costPerDeveloper, 12000);
     t.equal(c.productivity, 200);
 
     t.equal(c.morale,100);
-});
-
-test('city:', function (t) {
-    t.plan(10);
-
-    var c = null;
-    t.doesNotThrow(function () {
-        c = new city("dublin", 100, 200);
-    },'new city works');
-
+    
     t.equal(c.progress(0),0,'city progress works');
     t.ok(c.progress(1)>0,'city progress works');
 
     t.doesNotThrow(function(){
         c.cost();
-    },'city cost');
+    },'city cost')
+
     t.equals(c.status(),3,'city status');
 
     c.setMorale(120);
@@ -37,9 +65,10 @@ test('city:', function (t) {
     c.modifyMorale(-30);
     t.equals(c.morale,90,'city modifyMorale');
 
-    c.stall();
-    t.equals(c.status(),2,'city stall');
+    c.stall(10);
+    t.equals(c.stalled,10,'city stall');
 
     t.equals(c.idealProgress(0),0,'city idealProgress');
     t.equals(c.idealProgress(1),200,'city idealProgress');
+
 });
